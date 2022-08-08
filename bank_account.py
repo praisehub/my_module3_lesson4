41b804693e45aa36627585d61b7e824e70c3eae4
Praisehub-Git <praiseemmanuel9ic@gmail.com>
2022-08-08 12:29:08 +0100

    first commit

---
+++ bank_account.py
@@ -0,0 +1,35 @@
+#creating a bank account class
+class BankAccount():
+    
+    def __init__(self, name, number, type, balance):
+        self.name = name
+        self.number = number
+        self.type = type
+        self.balance = balance
+        
+acct_1 = BankAccount("Ali_John", "0056476666","Savings", "3000000")
+        
+print(acct_1.name)
+print(acct_1.number)
+print(acct_1.type)
+print(acct_1.balance)
+
+#creating methods
+class BankAccount:
+ def BankAccount(self,deposit, withdraw):
+  self.deposit = deposit
+  self.withdraw = withdraw
+       
+  print(self.deposit)
+  print(self.withdraw)
+    
+
+acct_1 = BankAccount (deposit = 600000, withdraw = 500000,)
+    
+
+        
+        
+        
+        
+
+    
